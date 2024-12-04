6e215a64909aec883266e40375b42b448572c3b8
Kirya32 <kirilkasungurov@yandex.ru>
2024-12-04 10:02:24 +0400

    pobeda

---
+++ Assets/Scripts/Worm.cs
@@ -0,0 +1,23 @@
+using System;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class Worm : Entity
+    {
+        [SerializeField] private int lives = 3;
+        
+        private void OnCollisionEnter2D(Collision2D collision)
+        {
+            if (collision.gameObject == Hero.Instance.gameObject)
+            {
+                Hero.Instance.GetDamage();
+                lives--;
+                Debug.Log("у червяка " + lives);
+            }
+            
+            if (lives < 1)
+                Die();
+        }
+    }
+}
